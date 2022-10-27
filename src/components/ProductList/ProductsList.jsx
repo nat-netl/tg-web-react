23bb5b81b027a90b7d0226bd2e40c785bd8a6ef7
nat-netl <nat_netl3@bk.ru>
2022-10-27 13:53:16 +0300

    catalog

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import './ProductsList.css'
+import React, { useState } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import ProductItem from "../ProductItem/ProductItem.JSX";
+import "./ProductsList.css";
 
-const ProductsList = () => {
-  return (
-    <div>ProductsList</div>
-  )
+const products = [
+  {id: '0', title: 'Куртка 1', price: 12000, description: 'Куртка прямого силуэта с центральной застежкой на «молнию», планкой на кнопках.'},
+  {id: '1', title: 'Куртка 2', price: 5220, description: 'Куртка прямого силуэта с центральной застежкой на «молнию», планкой на кнопках.'},
+  {id: '2', title: 'Куртка 5', price: 4900, description: 'Куртка прямого силуэта с центральной застежкой на «молнию», планкой на кнопках.'},
+  {id: '3', title: 'Куртка 3', price: 2200, description: 'Куртка прямого силуэта с центральной застежкой на «молнию», планкой на кнопках.'},
+  {id: '4', title: 'Куртка 6', price: 22000, description: 'Куртка прямого силуэта с центральной застежкой на «молнию», планкой на кнопках.'},
+  {id: '5', title: 'Куртка 7', price: 142000, description: 'Куртка прямого силуэта с центральной застежкой на «молнию», планкой на кнопках.'},
+  {id: '6', title: 'Куртка 4', price: 9999, description: 'Куртка прямого силуэта с центральной застежкой на «молнию», планкой на кнопках.'},
+]
+
+const getTotalPrice = (items = []) => {
+  return items.return ((acc, item) => {
+    return acc += item.price
+  }, 0)
 }
 
-export default ProductsList
+const ProductsList = () => {
+ 
+
+  const [addedItem, setAddedItem] = useState([]);
+  const {tg} = useTelegram();
+
+  const onAdd = (product) => {
+    const alreadyAdded = addedItem.find(item => item.id === product.id)
+    let newItems = [];
+
+    if (alreadyAdded) {
+      newItems = addedItem.filter(item => item.id !== product.id)
+    } else {
+      newItems = [...addedItem, product]
+    }
+
+    setAddedItem (newItems)
+
+    if (newItems.length === 0) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `Купить ${getTotalPrice(newItems)}`
+      });
+    }
+  };
+
+  return (
+    <div className={"list"}>
+      {products.map((product) => (
+        <ProductItem product={product} onAdd={onAdd} className={"item"} />
+      ))
+      }
+    </div>
+  );
+};
+
+export default ProductsList;
