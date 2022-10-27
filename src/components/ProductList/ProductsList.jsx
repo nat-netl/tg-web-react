a9744d9a566e598a2fce192dd4e2e92a4435d09e
nat-netl <nat_netl3@bk.ru>
2022-10-27 14:02:40 +0300

    fix file catalog

--- src/components/ProductList/ProductsList.jsx
+++ src/components/ProductList/ProductsList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
-import ProductItem from "../ProductItem/ProductItem.JSX";
+import ProductItem from "../ProductItem/ProductItem";
 import "./ProductsList.css";
 
 const products = [
